ad1517a399e532fd8ea1672148ecf7d5a27e87c4
chris@rosaloves.com <chris@rosaloves.com@a5c2a118-04db-11de-994d-8308d8631604>
2009-12-15 15:05:16 +0000

    [maven-release-plugin]  copy for tag bitlyj-1.0

---
+++ tags/bitlyj-1.0/src/main/java/com/rosaloves/net/shorturl/bitly/api/Api.java
@@ -0,0 +1,19 @@
+package com.rosaloves.net.shorturl.bitly.api;
+
+/**
+ * Api
+ * 
+ * $Id$
+ * 
+ * @author clewis Oct 6, 2009
+ *
+ */
+public interface Api {
+
+	public String getUrl();
+	
+	public String getEndPoint();
+	
+	public String getVersion();
+	
+}
